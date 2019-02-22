72217c86744d6bc91b584f12c1aaf4fce6f9d854
Yngve <michael.liljeback@gmail.com>
2019-02-22 14:05:27 +0100

    exchanged  reg.ex match for email validation in utils.py

--- src/common/utils.py
+++ src/common/utils.py
@@ -8,7 +8,7 @@ class Utils(object):
 
     @staticmethod
     def email_is_valid(email):
-        email_address_matcher = re.compile('^[\w-]+@([\w]+\.)+[\w]+$')
+        email_address_matcher = re.compile('(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)')
         return True if email_address_matcher.match(email) else False
 
     @staticmethod
